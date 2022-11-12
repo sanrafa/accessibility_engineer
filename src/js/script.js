b97b0fa080d059a1cba8154c5fca0d7cc2970d8e
Rafa Brown <shane.rafael@zohomail.com>
2022-11-12 16:51:39 -0700

    switch to js validation instead of html

--- src/js/script.js
+++ src/js/script.js
@@ -21,45 +21,95 @@ const formInputs = [
 
 const form = document.getElementById("form");
 
-const requiredError = "Error: This field is required.";
-const emailError = "Error: Please enter a valid email address.";
+const errors = {
+  fullName: "Error: Your full name is required.",
+  email: {
+    invalid: "Error: Please enter a valid email address.",
+    required: "Error: An email address is required.",
+  },
+  message: "Error: A message is required.",
+};
 
-formInputs.forEach((input) => {
+const errorBorderStyle = "solid 1px var(--red)";
+
+formInputs.forEach((field) => {
+  const [input, error] = field;
+
   if (
-    input[0] instanceof HTMLInputElement ||
-    input[0] instanceof HTMLTextAreaElement
+    input instanceof HTMLInputElement ||
+    input instanceof HTMLTextAreaElement
   ) {
-    input[0].addEventListener("change", () => {
-      /* if required field left blank */
-      if (input[0].validity.valueMissing) {
-        input[0].style.border = "solid 1px var(--red)";
-        input[1].innerText = requiredError;
+    input.addEventListener("blur", () => {
+      // if required field left blank
+      if (input.value.trim() === "") {
+        input.style.border = errorBorderStyle;
+        input.type === "text"
+          ? (error.innerText = errors.fullName)
+          : input.type === "email"
+          ? (error.innerText = errors.email.required)
+          : input.type === "textarea"
+          ? (error.innerText = errors.message)
+          : (error.innerText = "Error: This field is required.");
+      } else if (
+        // if invalid email
+        input.type === "email" &&
+        input.validity.typeMismatch
+      ) {
+        input.style.border = errorBorderStyle;
+        error.innerText = errors.email.invalid;
       } else {
-        input[0].style.border = "revert";
-        input[1].innerText = "";
-      }
-      /* If invalid email */
-      if (input[0].type === "email" && !input[0].validity.valueMissing) {
-        if (input[0].validity.typeMismatch) {
-          input[0].style.border = "solid 1px var(--red)";
-          input[1].innerText = emailError;
-        } else {
-          input[0].style.border = "revert";
-          input[1].innerText = "";
-        }
+        // input is valid
+        input.style.border = "revert";
+        error.innerText = "";
       }
     });
   }
 });
 
+function checkFormValidity() {
+  // map to boolean representing whether input is valid
+  // render error message if not
+  const validFields = formInputs.map((field) => {
+    const [input, error] = field;
+    if (input.value.trim() === "") {
+      input.style.border = errorBorderStyle;
+      input.setAttribute("aria-invalid", "true");
+      input.type === "text"
+        ? (error.innerText = errors.fullName)
+        : input.type === "email"
+        ? (error.innerText = errors.email.required)
+        : input.type === "textarea"
+        ? (error.innerText = errors.message)
+        : (error.innerText = "Error: This field is required.");
+      return false;
+    } else if (input.type === "email" && input.validity.typeMismatch) {
+      input.style.border = errorBorderStyle;
+      input.setAttribute("aria-invalid", "true");
+      error.innerText = errors.email.invalid;
+      return false;
+    } else {
+      input.setAttribute("aria-invalid", "false");
+      return true;
+    }
+  });
+
+  // check whether all inputs are valid
+  if (validFields.every((val) => val === true)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 if (form instanceof HTMLFormElement) {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const isValid = form.checkValidity();
+    const isValid = checkFormValidity();
     if (isValid) {
-      formInputs.forEach((input) => {
-        input[0].disabled = true;
-        input[1].innerText = "";
+      formInputs.forEach((field) => {
+        const [input, error] = field;
+        input.disabled = true;
+        error.innerText = "";
       });
       successMessage.style.display = "block";
       submitBtn.disabled = true;
